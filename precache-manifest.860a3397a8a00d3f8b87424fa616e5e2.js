ae36618cde26771974fe4028b84b454f295722cd
Szymon Janota <szymon.janota@sap.com>
2020-04-14 07:56:10 +0200

    Updates

---
+++ precache-manifest.860a3397a8a00d3f8b87424fa616e5e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a0861e294c433cbd04bed533006e37",
+    "url": "/factorio/index.html"
+  },
+  {
+    "revision": "87cfd44cdaf11bd2f8f1",
+    "url": "/factorio/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a8d6895ef16fcecb7517",
+    "url": "/factorio/static/css/main.4a9dbcc4.chunk.css"
+  },
+  {
+    "revision": "87cfd44cdaf11bd2f8f1",
+    "url": "/factorio/static/js/2.670e1fbe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/factorio/static/js/2.670e1fbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8d6895ef16fcecb7517",
+    "url": "/factorio/static/js/main.e82de6ad.chunk.js"
+  },
+  {
+    "revision": "85a08ed3ec0c0df6c5d2",
+    "url": "/factorio/static/js/runtime-main.73147325.js"
+  }
+]);
